0457f18842cb7e988d3eabd3b28beb9356f55ffa
Xolisa Nqimfa <s219737134@mandela.ac.za>
2022-09-13 11:56:11 +0200

    Add Student dashboard and Course Index

---
+++ TurboJsMVC/Controllers/Student/StudentCourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TurboJsMVC.Controllers.Student
+{
+    public class StudentCourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- TurboJsMVC/Controllers/Student/StudentDashboardController.cs
+++ TurboJsMVC/Controllers/Student/StudentDashboardController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TurboJsMVC.Models;
 
 namespace TurboJsMVC.Controllers.Student
 {
     public class StudentDashboardController : Controller
     {
+        private readonly GRP27ETutorContext _context;
+
+        public StudentDashboardController(GRP27ETutorContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult StudentDashboard()
         {
+            ViewData["Users"] = GetUsers();
+            ViewData["Courses"] = GetCourses();
             return View();
         }
+        private List<User> GetUsers()
+        {
+            List<User> users = new List<User>(_context.Users);
+            return users;
+        }
+        public List<Course> GetCourses()
+        {
+            List<Course> courses = new List<Course>(_context.Courses);
+            return courses;
+        }
+        
     }
 }

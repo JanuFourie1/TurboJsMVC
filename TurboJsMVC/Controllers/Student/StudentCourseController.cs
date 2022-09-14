b8d67c3134d4018fa92e87a45c7471eb5f1bd689
Xolisa Nqimfa <s219737134@mandela.ac.za>
2022-09-14 14:50:20 +0200

    Student Dashboard

--- TurboJsMVC/Controllers/Student/StudentCourseController.cs
+++ TurboJsMVC/Controllers/Student/StudentCourseController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TurboJsMVC.Models;
 
 namespace TurboJsMVC.Controllers.Student
 {
     public class StudentCourseController : Controller
     {
+        private readonly GRP27ETutorContext _context;
+
+        public StudentCourseController(GRP27ETutorContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public List<Course> GetCourses()
+        {
+            List<Course> courses = new List<Course>(_context.Courses);
+            return courses;
         }
     }
 }

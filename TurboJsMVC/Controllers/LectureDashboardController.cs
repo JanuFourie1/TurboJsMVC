a68b21030aea3c99c6704518d73259ebe0a37dfb
Somila Mihlali Lefume <s219602573@mandela.ac.za>
2022-10-18 11:47:04 +0200

    update dashboard

--- TurboJsMVC/Controllers/LectureDashboardController.cs
+++ TurboJsMVC/Controllers/LectureDashboardController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TurboJsMVC.Models;
 
 namespace TurboJsMVC.Controllers
 {
     public class LectureDashboardController : Controller
     {
-        public IActionResult LectureDashboard()
+        private readonly GRP27ETutorContext _context;
+
+        public LectureDashboardController(GRP27ETutorContext context)
         {
+            _context = context;
+            
+        }
+        public IActionResult LecturerDashboard()
+        {
+            ViewData["Users"] = GetUsers();
+            ViewData["Courses"] = GetModules();
             return View();
+        }
+        private List<User> GetUsers()
+        {
+            List<User> users = new List<User>(_context.Users);
+            return users;
+        }
+        public List<Module> GetModules()
+        {
+            List<Module> modules = new List<Module>(_context.Modules);
+            return modules;
         }
     }
 }
